8aaaf569c8c77b7e209a6d23b93436065e601037
Eko SW <swdev.bali@gmail.com>
2021-08-26 06:58:25 +0700

    release to pypi at https://pypi.org/project/latestearthquake-indonesia/0.1/

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="latestearthquake-indonesia",
-    version="0.3",
+    version="0.4",
     author="Eko S. Wibowo",
     author_email="coach@remoteworker.id",
     description="This package will get the latest earthquake from BMKG | Meteorological, Climatological, and "
